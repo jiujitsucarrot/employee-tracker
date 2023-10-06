e1c97b5dfa84811063cdeb6776c26acd3c0dbbcd
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-10-06 15:58:37 -0700

    Added prompts to server.js, need to debug. Isn't operating quite right.

--- server.js
+++ server.js
@@ -3,9 +3,64 @@ const departmentController = require('./controllers/departmentController');
 const roleController = require('./controllers/roleController');
 const employeeController = require('./controllers/employeeController');
 
-async function startApp() {
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            },
+        ])
+
+        .then((answers) => {
+            const choice = answers.choice;
+
+            switch (choice) {
+                case 'View all departments':
+                    departmentController.viewAllDepartments(mainMenu);
+                    break;
+                case 'View all roles':
+                    roleController.viewAllRoles(mainMenu);
+                    break;
+                case 'View all employees':
+                    employeeController.viewAllEmployees(mainMenu);
+                    break;
+                case 'Add a department':
+                    departmentController.addDepartment(mainMenu);
+                    break;
+                case 'Add a role':
+                    roleController.addRole(mainMenu);
+                    break;
+                case 'Add an employee':
+                    employeeController.addEmployee(mainMenu);
+                    break;
+                case 'Update an employee role':
+                    employeeController.updateEmployeeRole(mainMenu);
+                    break;
+                case 'Exit':
+                    console.log('Goodbye!');
+                    process.exit(0);
+                default:
+                    console.log('Invalid choice. Please try again.');
+                    mainMenu();
+            }
+        });
+}
+
+function startApp() {
     console.log('Welcome to the Employee Tracker!');
-    await mainMenu();
+    mainMenu();
 }
 
 startApp();
